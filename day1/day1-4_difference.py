d21df24d0a8c63a8e2446f1b5d6bdd5e545374e3
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-09-18 22:42:14 +0530

    A program to find the difference between two numbers.

---
+++ day1/day1-4_difference.py
@@ -0,0 +1,7 @@
+print("To find the difference of two numbers:")
+num1 = float(input("Enter bigger number: "))
+num2 = float(input("Enter smaller number: "))
+
+difference = num1 - num2
+
+print(difference)
