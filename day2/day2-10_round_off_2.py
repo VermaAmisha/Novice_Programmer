da202839c33a8a503d3d2e775f0a9cde446f08c1
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-09-19 23:23:22 +0530

    A program to round off any number up to two decimal places.

---
+++ day2/day2-10_round_off_2.py
@@ -0,0 +1,8 @@
+print("To find rounded off value after division:")
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+# round off to two decimal places
+div = round((a / b) , 2)
+
+print(div)
