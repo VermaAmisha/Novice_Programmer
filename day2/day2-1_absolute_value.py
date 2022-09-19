cf70c183c29277738a4cc171754cc8f3fb49b412
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-09-19 22:08:53 +0530

    A program to find absolute values of different numbers.

---
+++ day2/day2-1_absolute_value.py
@@ -0,0 +1,11 @@
+num1 = -12.5
+num2 = 26
+
+# Returns absolute values of numbers
+
+absolute_value1 = abs(num1)
+absolute_value2 = abs(num2)
+
+# To separate both the values using "|"
+
+print(absolute_value1 , absolute_value2 , sep=" | " )
