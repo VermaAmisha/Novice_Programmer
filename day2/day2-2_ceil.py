2593daddf8069d88c7c914370a1ccb88588d094f
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-09-19 22:17:05 +0530

    A program to find the ceiling value of numbers.

---
+++ day2/day2-2_ceil.py
@@ -0,0 +1,10 @@
+from math import ceil
+
+a = -23.56
+b = 56.89
+
+# returns the bigger/near value of the numbers
+
+ceiling_value1 = ceil(a)
+ceiling_value2 = ceil(b)
+print(ceiling_value1 , ceiling_value2 , sep="\n")
