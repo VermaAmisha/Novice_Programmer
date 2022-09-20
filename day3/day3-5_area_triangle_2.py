1ffa62e66d64dfdded0387358870bc6474354095
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-09-20 23:50:23 +0530

    A program to find area of triangle using its height.

---
+++ day3/day3-5_area_triangle_2.py
@@ -0,0 +1,9 @@
+
+print("To find the area of a triangle:")
+
+base = float(input("Enter length of base in cm: "))
+height = float(input("Enter height in cm: "))
+
+area = 1/2 * base * height
+
+print(area,"sq cm")
