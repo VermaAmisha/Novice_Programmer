b89dcdf3f54628aefcc6185405f98a636c395c4d
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-09-20 23:58:11 +0530

    A program to find volume of a cuboid.

---
+++ day3/day3-8_volume_cuboid.py
@@ -0,0 +1,8 @@
+print("To find the volume of a cuboid:")
+
+l = float(input("Enter length in cm: "))
+b = float(input("Enter breadth in cm: "))
+h = float(input("Enter height in cm: "))
+
+volume = l * b * h
+print(volume,"sq cm")
