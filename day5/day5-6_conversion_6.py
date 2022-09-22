354be0d436a282b33f5b8c82e1f5772cb53c0be5
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-09-22 23:57:57 +0530

    A program to convert kilometer to meter and vice versa

---
+++ day5/day5-6_conversion_6.py
@@ -0,0 +1,13 @@
+print("To convert Kilometer to Meter:")
+
+km = float(input("Enter the length in km: "))
+conversion = km * 1000
+
+print("The length in m:" , conversion)
+
+print("To convert Meter to Kiloeter:")
+
+m = float(input("Enter the length in m: "))
+conversion = km / 1000
+
+print("The length in km:" , conversion)
