6fb52353c68b5b59ea43393183848590a2b0df8d
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-09-23 18:47:43 +0530

    A program to convert inches to cm and vice versa.

---
+++ day6/day6-2_1_conversion.py
@@ -0,0 +1,15 @@
+print("To convert inches into centimeters:")
+
+inches = float(input("Enter the value in inches: "))
+# 1 inch = 2.5 cm
+
+conversion = inches * 2.54
+print("The value in cm is:", conversion)
+
+print("To convert centimeters into inches:")
+
+cm = float(input("Enter the value in centimeters(cm): "))
+# 1 inch = 2.5 cm
+
+conversion = cm / 2.54
+print("The value in inches is:", conversion)
