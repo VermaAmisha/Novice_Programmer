3b903bbbb0ae415d176a4f9a4e2716e7aaa083d8
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-09-23 23:15:34 +0530

    A program to swap numbers without using third variable.

---
+++ day6/day6-5_swap_nos_2.py
@@ -0,0 +1,11 @@
+print("To swap the variable values:")
+
+a = int(input("Enter a whole number for variable 'a': "))
+b = int(input("Enter a whole number for variable 'b': "))
+
+# swapping the values without using third variable
+
+a , b = b , a
+
+print("Value of 'a' after swapping:" , a)
+print("Value of 'b' after swapping:" , b)
