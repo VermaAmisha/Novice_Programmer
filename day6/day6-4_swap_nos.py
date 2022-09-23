f7982996db86c0ee127c19df9e16ad6d882ba2a7
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-09-23 23:14:16 +0530

    A program to swap numbers using third variable.

---
+++ day6/day6-4_swap_nos.py
@@ -0,0 +1,13 @@
+print("To swap variable values:")
+
+a = int(input("Enter a whole numbrer for variable 'a': "))
+b = int(input("Enter another whole number for variable 'b': "))
+
+# another variable "v" created to swap the values
+
+v = a
+a = b
+b = v
+
+print("value for 'a' now:" , a)
+print("value for 'b' now:" , b)
