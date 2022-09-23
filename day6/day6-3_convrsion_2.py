15138cf4fe27e860b3058a80150c4116315c4cb0
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-09-23 22:48:19 +0530

    A program to convert feet into meters and vice versa

---
+++ day6/day6-3_convrsion_2.py
@@ -0,0 +1,14 @@
+print("To convert feet into meters:")
+
+feet = float(input("Enter the value in feet: "))
+# 1 feet = 0.3048 m
+
+conversion = feet * 0.3048
+print("The value in m is:", conversion)
+
+print("To convert meters into feet:")
+
+m = float(input("Enter the value in meters(m): "))
+
+conversion = m / 0.3048
+print("The value in feet is:", conversion)
