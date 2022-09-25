5551fe6936e67ea10595ba707a6c7e626d094511
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-09-25 19:23:11 +0530

    A program using if else and 'is' operator

---
+++ day8/day8-2_if_else_2.py
@@ -0,0 +1,6 @@
+x = ("A","B","C")
+y = ("B","C","A")
+if x is y:
+    print("Not in doing what you like , but in liking what you do , is the secret of happiness!")
+else:
+    print("Life is what happens when you are busy making other plans!")
