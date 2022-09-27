dc46696a3cb6610d13b7eb66273c34e47abf6b1b
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-09-27 23:33:19 +0530

    A program to show the uses of variables.

---
+++ day10/day10-1_use_of_variables.py
@@ -0,0 +1,9 @@
+character_name="Amisha"
+character_age = "23"
+#printing
+print("There once was a girl named " + character_name + ",") #using variable name in the print sstatement 
+print("she was " + character_age + " years old.") #using variable in the ptint statement
+#Changing the value of variable
+character_name="Mini"
+print("She liked being called "+ character_name +",")
+print("and was too cool for " + character_age + " year olds.")
