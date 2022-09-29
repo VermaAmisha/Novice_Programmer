9650f179effcd7b8bb5b74efe4213545716e038f
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-09-29 21:38:04 +0530

    A mad libs game program.

---
+++ day12/day12-2_greater_number.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter the first Number: "))
+num2=int(input("Enter the Second Number: "))
+
+if num1 > num2:
+    print("Greater nubmer is:", num1)
+else:
+    print("Greater nubmer is:", num2)
