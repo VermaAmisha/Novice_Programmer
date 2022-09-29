762fec8ecfbdda9bc548c38b8317cd284ddc000c
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-09-29 21:38:25 +0530

    Delete day12-2_greater_number.py

--- day12/day12-2_greater_number.py
+++
@@ -1,7 +0,0 @@
-num1=int(input("Enter the first Number: "))
-num2=int(input("Enter the Second Number: "))
-
-if num1 > num2:
-    print("Greater nubmer is:", num1)
-else:
-    print("Greater nubmer is:", num2)
