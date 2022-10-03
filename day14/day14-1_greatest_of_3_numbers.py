08c894f71a61add06003ff42b86d3771f6ecde90
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-10-03 10:13:44 +0530

    A program to find greatest number out of any three numbers.

---
+++ day14/day14-1_greatest_of_3_numbers.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+num3=int(input("Enter the third number: "))
+if num1>=num2 and num1>=num3:
+    print("Gretest number: ",num1)
+if num2>=num1 and num2>=num3:
+    print("Gretest number: ",num2)
+if num3>=num2 and num3>=num1:
+    print("Gretest number: ",num3)
